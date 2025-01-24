3526d23b3fabbd95c234ba03f4b8a19ffebe7db5
halilcanciger1 <149325624+halilcanciger1@users.noreply.github.com>
2025-01-24 17:50:47 +0300

    Proje dosyası ekle.

---
+++ OnlineEdu.DataAccess/Context/OnlineEduContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineEdu.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.DataAccess.Context
+{
+    public class OnlineEduContext: DbContext
+    {
+        public OnlineEduContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+
+        public DbSet<About> Abouts { get; set; }
+
+        public DbSet<Banner> Banners { get; set; }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<BlogCategory> BlogsCategories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Course> Course { get; set; }
+
+        public DbSet<CourseCategory> CourseCategories { get; set; }
+
+        public DbSet<Subscriber> Subscribers { get; set; }
+
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
