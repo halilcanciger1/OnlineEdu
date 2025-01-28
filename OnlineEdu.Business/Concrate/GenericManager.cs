e55a5eeeb0466b9fe8b790fbb1cf550d0a85c064
halilcanciger1 <149325624+halilcanciger1@users.noreply.github.com>
2025-01-28 14:32:39 +0300

    initial commit

---
+++ OnlineEdu.Business/Concrate/GenericManager.cs
@@ -0,0 +1,59 @@
+﻿using OnlineEdu.Business.Abstract;
+using OnlineEdu.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.Business.Concrate
+{
+    public class GenericManager<T>(IRepository<T> repository) : IGenericService<T> where T : class
+    {
+        public int TCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TCreate(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TFilteredCount(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T TGetByFilter(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> TGetFilteredList(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OnlineEdu.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineEdu.DataAccess.Abstract;
+using OnlineEdu.DataAccess.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.DataAccess.Repositories
+{
+    public class GenericRepository<T>(IRepository<T> repository) : IRepository<T> where T : class
+    {
+
+        public int Count()
+        {
+            return repository.Count();
+        }
+
+        public void Create(T entity)
+        {
+            repository.Create(entity);
+        }
+
+        public void Delete(int id)
+        {
+            repository.Delete(id);
+        }
+
+        public int FilteredCount(Expression<Func<T, bool>> predicate)
+        {
+            return repository.FilteredCount(predicate);
+        }
+
+        public T GetByFilter(Expression<Func<T, bool>> predicate)
+        {
+            return repository.GetByFilter(predicate);
+        }
+
+        public T GetById(int id)
+        {
+            return repository.GetById(id);
+        }
+
+        public List<T> GetFilteredList(Expression<Func<T, bool>> predicate)
+        {
+            return repository.GetFilteredList(predicate);
+        }
+
+        public List<T> GetList()
+        {
+            return repository.GetList();
+        }
+
+        public void Update(T entity)
+        {
+            repository.Update(entity);
+        }
+    }
+}
